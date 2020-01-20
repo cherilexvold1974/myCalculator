08e01303d19e4303f86606851b59639c066f3a9b
Cherolyn Lexvold <ahavalexvold@gmail.com>
2020-01-20 06:18:26 -0600

    main.go: Experimentation with addition was successful.
    Takes addition as a first step in causing the program to calculate.
    Calulation using four operators is a goal.
    
    This patch successfully implements addition to the process.
    Signed-off-by: Cherolyn Lexvold <ahavalexvold@gmail.com>

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import(
 	"fmt"
 )
 
-func calculate(a int, b int) (out int){
-	fmt.Println(a+b)
-	return a+b
+func calculate(a int, operator string, b int){
+	if operator == "+"{
+	     fmt.Println(a+b)
+	}
 }
-
+                 
 func main() {
 	fmt.Println("Hello world")
-	calculate(12,2)
+	calculate(12, "+", 2)
 }
 
 		
