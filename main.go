4a7c53f3ebbf0aea7610627434da01f77c08cbb8
Cherolyn Lexvold <ahavalexvold@gmail.com>
2020-02-10 07:16:16 -0600

    main.go: Implemented subtraction into myCalculator.
    myCalculator requires the function of subtraction.
    Subtaction is a necessary function of this calculator.
    
    This patch implements subtraction into main.go.
    
    Signed-off-by: Cherolyn Lexvold <ahavalexvold@gmail.com>

--- main.go
+++ main.go
@@ -7,12 +7,15 @@ import(
 func calculate(a int, operator string, b int){
 	if operator == "+"{
 	     fmt.Println(a+b)
+	} else if operator == "-"{
+		 fmt.Println(a-b)
 	}
 }
                  
 func main() {
 	fmt.Println("Hello world")
 	calculate(12, "+", 2)
+	calculate(33, "-", 5)
 }
 
 		
