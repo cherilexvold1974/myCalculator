096562dfb53f5a95b937618cfb1be1027d6158d4
Cherolyn Lexvold <ahavalexvold@gmail.com>
2020-02-10 20:29:27 -0600

    main.go: Provided an error message in the case under division, where b=0.
    The case arises at times when division by zero is
    attempted. This case requires an error message.
    
    This patch actualizes an error message for division by
    zero.
    
    Signed-off-by: Cherolyn Lexvold <ahavalexvold@gmail.com>

--- main.go
+++ main.go
@@ -12,16 +12,20 @@ func calculate(a int, operator string, b int){
 	} else if operator == "*"{
 		fmt.Println(a*b)
 	} else if operator == "/"{
+		 if b == 0{
+	 	 fmt.Println("error: b cannot = 0")
+	} else{
 		fmt.Println(a/b)
 	}
 }
-                 
+}                 
 func main() {
 	fmt.Println("Hello world")
 	calculate(12, "+", 2)
 	calculate(33, "-", 5)
 	calculate(6, "*", 3)
 	calculate(12, "/", 4)
+	calculate(5, "/", 0)
 }
 
 		
